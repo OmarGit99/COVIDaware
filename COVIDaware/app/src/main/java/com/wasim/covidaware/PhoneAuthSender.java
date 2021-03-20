6a5a2d64e20f2f0c0d6c394c3d41dd935736b362
wjasani <jasaniwasim2002@gmail.com>
2021-03-20 09:55:28 +0530

    login coding integrated

---
+++ COVIDaware/app/src/main/java/com/wasim/covidaware/PhoneAuthSender.java
@@ -0,0 +1,124 @@
+package com.wasim.covidaware;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+import static android.content.ContentValues.TAG;
+
+public class PhoneAuthSender {
+
+    private static String mVerificationId;
+    private static PhoneAuthProvider.ForceResendingToken mResendToken;
+
+    public static void sendVerificationCode(String mobile, Context con) {
+        c = con;
+        Toast.makeText(con, "sending verification code", Toast.LENGTH_SHORT).show();
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(mobile)       // Phone number to verify
+                        .setTimeout(120L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity((Activity) c)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+    private static void verifyVerificationCode(String code) {
+        //creating the credential
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, code);
+
+        //signing the user
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    static Context c ;
+
+    private static PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential credential) {
+            // This callback will be invoked in two situations:
+            // 1 - Instant verification. In some cases the phone number can be instantly
+            //     verified without needing to send or enter a verification code.
+            // 2 - Auto-retrieval. On some devices Google Play services can automatically
+            //     detect the incoming verification SMS and perform verification without
+            //     user action.
+            Log.d(TAG, "onVerificationCompleted:" + credential);
+
+            signInWithPhoneAuthCredential(credential);
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            // This callback is invoked in an invalid request for verification is made,
+            // for instance if the the phone number format is not valid.
+            Log.w(TAG, "onVerificationFailed", e);
+
+            if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                // Invalid request
+            } else if (e instanceof FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+            }
+
+            // Show a message and update the UI
+        }
+
+        @Override
+        public void onCodeSent(@NonNull String verificationId,
+                               @NonNull PhoneAuthProvider.ForceResendingToken token) {
+            // The SMS verification code has been sent to the provided phone number, we
+            // now need to ask the user to enter the code and then construct a credential
+            // by combining the code with a verification ID.
+            Log.d(TAG, "onCodeSent:" + verificationId);
+
+            // Save verification ID and resending token so we can use them later
+            mVerificationId = verificationId;
+            mResendToken = token;
+        }
+    };
+
+    static FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    private static void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener((Activity) c, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //verification successful we will start the profile activity
+                            Toast.makeText(c, "Logged in" , Toast.LENGTH_SHORT).show();
+
+                        } else {
+
+                            //verification unsuccessful.. display an error message
+
+                            String message = "Something is wrong, we will fix it soon...";
+
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                message = "Invalid code entered...";
+                            }
+
+                            Toast.makeText((Activity)c, ""+message, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+}
---
+++ COVIDaware/app/src/main/java/com/wasim/covidaware/safetynet.java
@@ -0,0 +1,102 @@
+package com.wasim.covidaware;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.safetynet.SafetyNet;
+import com.google.android.gms.safetynet.SafetyNetApi;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import static android.content.ContentValues.TAG;
+
+
+public class safetynet {
+
+    public static void checkEnabled(Context c) {
+        SafetyNet.getClient(c)
+                .isVerifyAppsEnabled()
+                .addOnCompleteListener(new OnCompleteListener<SafetyNetApi.VerifyAppsUserResponse>() {
+                    @Override
+                    public void onComplete(Task<SafetyNetApi.VerifyAppsUserResponse> task) {
+                        if (task.isSuccessful()) {
+                            SafetyNetApi.VerifyAppsUserResponse result = task.getResult();
+                            if (result.isVerifyAppsEnabled()) {
+                                Log.d("MY_APP_TAG", "The Verify Apps feature is enabled.");
+                                verify(c);
+                               // MainActivity m = new MainActivity();m.verify();
+                            } else {
+                                enable(c);
+                            }
+                        } else {
+                            Log.e("MY_APP_TAG", "A general error occurred.");
+                        }
+                    }
+                });
+    }
+
+    static int t = 0;
+
+    private static void enable(Context c) {
+        SafetyNet.getClient(c)
+                .enableVerifyApps()
+                .addOnCompleteListener(new OnCompleteListener<SafetyNetApi.VerifyAppsUserResponse>() {
+                    @Override
+                    public void onComplete(Task<SafetyNetApi.VerifyAppsUserResponse> task) {
+                        if (task.isSuccessful()) {
+                            SafetyNetApi.VerifyAppsUserResponse result = task.getResult();
+                            if (result.isVerifyAppsEnabled()) {
+                                Log.d("MY_APP_TAG", "The user gave consent " +
+                                        "to enable the Verify Apps feature.");
+                                verify(c);
+                            } else {
+                                if (t > 2)
+                                    ((Activity) c).finish();
+                                Toast.makeText(c, "Please Enable c Setting to Proceed Further", Toast.LENGTH_LONG).show();
+                                enable(c);
+                                t++;
+                            }
+                        } else {
+                            Log.e("MY_APP_TAG", "A general error occurred.");
+                        }
+                    }
+                });
+    }
+
+    public static void verify(Context c) {
+        SafetyNet.getClient((Activity)c).verifyWithRecaptcha("6Lez24YaAAAAAJpQL50xj5M9PQDQJ08XqN8iGfy2")
+                .addOnSuccessListener((Activity)c, new OnSuccessListener<SafetyNetApi.RecaptchaTokenResponse>() {
+                    @Override
+                    public void onSuccess(SafetyNetApi.RecaptchaTokenResponse recaptchaTokenResponse) {
+                        String userResponseToken = recaptchaTokenResponse.getTokenResult();
+                        com.wasim.covidaware.PhoneAuthSender.sendVerificationCode("+919867735957", (Activity)c);
+                    }
+                })
+                .addOnFailureListener((Activity)c, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        if (e instanceof ApiException) {
+                            // An error occurred when communicating with the
+                            // reCAPTCHA service. Refer to the status code to
+                            // handle the error appropriately.
+                            ApiException apiException = (ApiException) e;
+                            int statusCode = apiException.getStatusCode();
+                            Log.d(TAG, "Error: " + CommonStatusCodes
+                                    .getStatusCodeString(statusCode));
+                        } else {
+                            // A different, unknown type of error occurred.
+                            Log.d(TAG, "Error: " + e.getMessage());
+                        }
+                    }
+                });
+    }
+
+}
